a7a47155c6dbe0ee3ef442ebc604d1f9debf5a6b
raja <kunchala@yahoo.com>
2021-10-23 23:48:08 +0100

    Added tests for Cocktails
    Initial commit for Cocktails

---
+++ src/test/java/com/techreturners/apilab1/controller/CocktailsControllerTests.java
@@ -0,0 +1,47 @@
+package com.techreturners.apilab1.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CocktailsControllerTests {
+
+    @Autowired
+
+    private MockMvc mockMvcController;
+
+    @Test
+    public void testGetCocktailWithoutRequestParams() throws Exception {
+
+        String expectedValue = "Old Fashioned";
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/cocktails"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name")
+                        .value(expectedValue));
+
+    }
+
+    @Test
+    public void testGetCoffeeWithRequestParams() throws Exception {
+
+        String expectedValue = "Negroni";
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/cocktails").param("name",
+                                "Negroni"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name")
+                        .value(expectedValue));
+
+    }
+
+
+}
